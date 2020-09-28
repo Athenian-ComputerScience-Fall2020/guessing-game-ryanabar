7fbd4e7c7fdf6e11843878dade9b5bc7b34edc50
ryanabar <21rabar@athenian.org>
2020-09-28 21:50:43 +0000

    Final Cosmetic Changes

--- game1.py
+++ game1.py
@@ -1,39 +1,47 @@
 '''
 Collaborators: None
-
-Use this file to write an "open" version of the game (no test code or defined format). This will be translated into a testable program later. Use the reponse statements provided to make that transition easier.
-
-"Your number is too high."
-"Your number is too low."
-"Your number is out of range."
-"I'm sorry you are giving up!"
-"I'm sorry, you are out of guesses."
-
 '''
 
-import random
+def rerun(): 
+	while True: 
+		guessing_game() 
 
-x = random.randint(1,11)
+def guessing_game():        
+    import random
 
-try:
-    number_of_guesses = 0
-    while number_of_guesses < 5:
-        guess = int(input("Guess a number between 1 and 10 or press 'q' to quit: "))
-        number_of_guesses = number_of_guesses + 1
-        if number_of_guesses == 5:
+    low = int(input("Enter a minimum boundary for the range of numbers: "))
+    high = int(input("Enter a maximum boundary for the range of numbers: "))
+    x = random.randint(low, high)
+
+    try:
+        number_of_guesses = 0
+        while number_of_guesses < 1000000000000000000:
+            guess = int(input("Guess a number between your minimum and your maximum boundaries or press 'q' to quit: "))
+            number_of_guesses = number_of_guesses + 1
+            if guess == str("q"):
+                break
+            if guess > x:
+                print("Your number is too high.")
+            elif guess < x:
+                print("Your number is too low.")
+            else:
+                print("Your guess is correct!")
+                break
+    except:
+        print("Please enter a number.")
+
+if __name__ == '__main__': 
+    rerun() 
+
+
+
+'''
+I no longer need this code because you can't run out of guesses anymore.
+
+if number_of_guesses == 5:
             print("I'm sorry, you are out of guesses.")
             print("\n")
         if number_of_guesses == 4:
             print("I'm sorry you are giving up!")
             print("\n")
-        if guess == str("q"):
-            break
-        if guess > x:
-            print("Your number is too high.")
-        elif guess < x:
-            print("Your number is too low.")
-        else:
-            print("Your guess is correct!")
-            break
-except:
-    print("Please enter a number.")
+'''
