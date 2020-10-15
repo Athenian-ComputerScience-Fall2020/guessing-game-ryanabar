432cebed6d80c19d79c9f96218180910be7f00db
ryanabar <21rabar@athenian.org>
2020-10-15 16:52:44 +0000

    FINAL

--- game1.py
+++ game1.py
@@ -1,39 +1,39 @@
 '''
-Collaborators: None
+Collaborators: Megan
 '''
 
-def guessing_game():        
+def guessing_game(): #Defining my function
     import random
 
-    low = int(input("Enter a minimum boundary for the range of numbers: "))
-    high = int(input("Enter a maximum boundary for the range of numbers: "))
+    low = int(input("Enter a minimum boundary for the range of numbers: ")) #Lower boundary input from user
+    high = int(input("Enter a maximum boundary for the range of numbers: ")) #Upper boundary input from user
     x = random.randint(low, high)
 
     try:
-        max_guess = int(input("How many guesses would you like to have? "))
+        max_guess = int(input("How many guesses would you like to have? ")) #Number of guesses input from user
         number_of_guesses = 0
         while number_of_guesses < max_guess:
-            guess = int(input("Guess a number between your minimum and your maximum boundaries or press 'q' to quit: "))
+            guess = int(input("Guess a number between your minimum and your maximum boundaries or press 'q' to quit: ")) #Where user enters their guess
             number_of_guesses = number_of_guesses + 1
-            if guess == str("q"):
+            if guess == str("q"): #Quits the program
                 break
-            if guess > x:
+            if guess > x: #Guess is too high
                 print("Your number is too high.")
-            elif guess < x:
+            elif guess < x: #Guess is too low
                 print("Your number is too low.")
-            else:
+            else: #Correct guess
                 print("Your guess is correct!")
                 break
-    except:
+    except: #If the user does not enter a number...
         print("Please enter a number.")
 
 if __name__ == '__main__': 
-    guessing_game()
+    guessing_game() #Calling the function
     while True:
-        a = input("Play again? Please say yes or no. ")
-        if a == "yes":
+        a = input("Play again? Please say yes or no. ") #Option to play again
+        if a == "yes": #Plays again
             guessing_game()
-        if a == "no":
+        if a == "no": #Does not play again
             break
 
 
